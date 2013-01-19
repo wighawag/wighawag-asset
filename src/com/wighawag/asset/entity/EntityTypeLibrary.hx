5670edf9050bfce549d6957e56609c77162e2901
Wighawag <wighawag@gmail.com>
2013-01-19 08:53:08 +0530

    use xml for entity types and fix asset loader when multiple sprite share same atlas

--- src/com/wighawag/asset/entity/EntityTypeLibrary.hx
+++ src/com/wighawag/asset/entity/EntityTypeLibrary.hx
@@ -1,25 +1,54 @@
 package com.wighawag.asset.entity;
 
+import haxe.xml.Fast;
+import com.wighawag.system.EntityTypeComponent;
 import com.wighawag.system.EntityType;
 
 class EntityTypeLibrary {
 
     private var types : Hash<EntityType>;
 
-    // TODO use xml ?
-    public function new(types : Array<EntityType>) {
-        this.types = new Hash();
-        for(type in types){
-            if(this.types.exists(type.id)){
-                Report.anError("EntityTypeLibrary", "EntityType with id " + type.id + " already exist in the library.");
+
+    public function new(xml : String){//TODO? : }, componentFactory : EntityTypeComponentFactory) {
+        types = new Hash();
+
+        var x = new Fast( Xml.parse(xml).firstElement() );
+        for (typeDefinition in x.nodes.type){
+            var typeId : String = typeDefinition.att.id;
+
+            var components : Array<EntityTypeComponent> = new Array();
+            for (componentDefinition in typeDefinition.elements){
+                var className = componentDefinition.name;
+                //TODO have className find their package in some setup in the xml or another one ?
+                //TODO? use componentFactory if available
+                var clazz = Type.resolveClass(className);
+                // TODO find a better mechanism to initialise components:
+                var instance : Dynamic = null;
+                try{
+                    instance = Type.createInstance(clazz, [componentDefinition.x]); // assume the component has an empty constructor
+                    components.push(instance);
+                }catch(e : Dynamic){
+                    Report.anError("EntityTypeLibrary", "EntityType with id " + typeId + " has a component that cannot be instanciated : " + className, "make sure this component has an constrcutor acepting one xml (Xml) argument");
+                }
+
+            }
+
+            if(types.exists(typeId)){
+                Report.anError("EntityTypeLibrary", "EntityType with id " + typeId + " already exist in the library.");
             }else{
-                this.types.set(type.id, type);
+                var type = new EntityType(typeId);
+                type.setup(components);
+                types.set(typeId, type);
             }
         }
     }
 
-    public function get(typeName : String) : EntityType{
-        return types.get(typeName);
+    public function get(typeId : String) : EntityType{
+        var type = types.get(typeId);
+        if(type == null){
+            Report.anError("EntityTypeLibrary", "No EntityType with id " + typeId);
+        }
+        return type;
     }
 
 }
--- src/com/wighawag/asset/spritesheet/SpriteLibrary.hx
+++ src/com/wighawag/asset/spritesheet/SpriteLibrary.hx
@@ -1,14 +1,21 @@
 package com.wighawag.asset.spritesheet;
 import com.wighawag.asset.load.TextAsset;
 import com.wighawag.asset.load.Batch;
+import com.wighawag.asset.load.AssetManager;
 import com.wighawag.report.Report;
 import haxe.xml.Fast;
 import promhx.Promise;
+
+typedef WaitingSprite = {
+id : AssetId,
+promise : Promise<Sprite>
+}
+
 class SpriteLibrary {
 
     private var textureAtlasLibrary : TextureAtlasLibrary;
     private var spriteDefinitions : Hash<Fast>;
-    private var textureAtlasSpriteMap : Hash<Array<Promise<Sprite>>>;
+    private var textureAtlasSpriteMap : Hash<Array<WaitingSprite>>;
     private var promises : Hash<Promise<Sprite>>;
     private var batchPromises : Hash<Promise<Batch<Sprite>>>;
 
@@ -28,61 +35,66 @@ class SpriteLibrary {
 
 
     public function fetch(id : String) : Promise<Sprite>{
+        var spriteDefinition = spriteDefinitions.get(id);
+        if (spriteDefinition == null){
+            Report.anError("SpriteLibrary", "Sprite does not exist : ", id);
+            return null;
+        }
+
         var promise = promises.get(id);
         if (promise == null){
             promise = new Promise();
-
-            var spriteDefinition = spriteDefinitions.get(id);
-            if (spriteDefinition == null){
-                Report.anError("SpriteLibrary", "Sprite does not exist : ", id);
-                return null;
-            }
 
             promises.set(id, promise);
             var textureAtlasId = spriteDefinition.att.textureAtlas;
             var spriteList = textureAtlasSpriteMap.get(textureAtlasId);
             if (spriteList == null){
-                spriteList = new Array<Promise<Sprite>>();
+                spriteList = new Array<WaitingSprite>();
+                spriteList.push({id:id,promise:promise});
                 textureAtlasSpriteMap.set(textureAtlasId, spriteList);
+                textureAtlasLibrary.fetch(textureAtlasId).then(resolveWaitingSprites);
+            }else{
+                spriteList.push({id:id,promise:promise});
             }
-            spriteList.push(promise);
-            textureAtlasLibrary.fetch(textureAtlasId).then(function (textureAtlas : TextureAtlas) : Void{
-                var spriteList = textureAtlasSpriteMap.get(textureAtlas.id);
-                for (spritePromise in spriteList){
-                    var subTexture : SubTexture = null;
-                    var animations : Hash<Animation> = new Hash();
-                    for (animationDef in spriteDefinition.nodes.animation){
-                        var frames = new Array<Frame>();
-                        for (frameDef in animationDef.nodes.frame){
-                            var overrideDuration = 0;
-                            if (frameDef.has.overrideDuration){
-                                overrideDuration = Std.parseInt(frameDef.att.overrideDuration);
-                            }
-                            var frameX = 0;
-                            if (frameDef.has.x){
-                                frameX = Std.parseInt(frameDef.att.x);
-                            }
-                            var frameY = 0;
-                            if (frameDef.has.y){
-                                frameY = Std.parseInt(frameDef.att.y);
-                            }
-                            subTexture = textureAtlas.textures.get(frameDef.att.subTexture);
-                            frames.push(new Frame(subTexture,frameX, frameY, overrideDuration));
-                        }
-                        var loopStartFrame = 0;
-                        if (animationDef.has.loopStartFrame){
-                            loopStartFrame = Std.parseInt(animationDef.att.loopStartFrame);
-                        }
-                        animations.set(animationDef.att.id, new Animation(animationDef.att.id, frames, Std.parseInt(animationDef.att.defaultDuration), loopStartFrame));
-                    }
-
-                    spritePromise.resolve(new Sprite(id, textureAtlas, subTexture.bitmapAsset, animations));
-                }
-                textureAtlasSpriteMap.remove(textureAtlas.id);
-            });
         }
 
         return promise;
+    }
+
+    private function resolveWaitingSprites(textureAtlas : TextureAtlas) : Void{
+        var spriteList = textureAtlasSpriteMap.get(textureAtlas.id);
+        for (waitingSprite in spriteList){
+            var spriteDef = spriteDefinitions.get(waitingSprite.id);
+            var subTexture : SubTexture = null;
+            var animations : Hash<Animation> = new Hash();
+            for (animationDef in spriteDef.nodes.animation){
+                var frames = new Array<Frame>();
+                for (frameDef in animationDef.nodes.frame){
+                    var overrideDuration = 0;
+                    if (frameDef.has.overrideDuration){
+                        overrideDuration = Std.parseInt(frameDef.att.overrideDuration);
+                    }
+                    var frameX = 0;
+                    if (frameDef.has.x){
+                        frameX = Std.parseInt(frameDef.att.x);
+                    }
+                    var frameY = 0;
+                    if (frameDef.has.y){
+                        frameY = Std.parseInt(frameDef.att.y);
+                    }
+                    subTexture = textureAtlas.textures.get(frameDef.att.subTexture);
+                    frames.push(new Frame(subTexture,frameX, frameY, overrideDuration));
+                }
+                var loopStartFrame = 0;
+                if (animationDef.has.loopStartFrame){
+                    loopStartFrame = Std.parseInt(animationDef.att.loopStartFrame);
+                }
+                animations.set(animationDef.att.id, new Animation(animationDef.att.id, frames, Std.parseInt(animationDef.att.defaultDuration), loopStartFrame));
+            }
+
+            waitingSprite.promise.resolve(new Sprite(waitingSprite.id, textureAtlas, subTexture.bitmapAsset, animations));
+        }
+        textureAtlasSpriteMap.remove(textureAtlas.id);
     }
 
 
